7ec2fe092a8ebf4de062bcfabae9dd66613f94bf
wiljar00 <wiljar00@gmail.com>
2018-03-26 13:54:15 -0600

    button
    Submit button class.

---
+++ src/components/Button.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import {
+    StyleSheet,
+    Text,
+    TouchableOpacity
+} from 'react-native';
+import styles from '../styles/styles';
+
+export default class CustomButton extends Component {
+
+    render() {
+        return (
+            // Create a touchable object and accept passed function
+            <TouchableOpacity
+                style={styles.button}
+                onPress = {this.props.onPress}
+            >
+            {/* Output the passed name on each button */}
+            <Text
+                style={StyleSheet.flatten(styles.buttonText)}>
+                {this.props.name}
+            </Text>
+            </TouchableOpacity>
+        );
+    }
+}
