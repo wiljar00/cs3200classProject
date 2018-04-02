b5a8aac8b35e7204fdfa62f0be512b79d3ae4226
evatar7 <evan.burke@aggiemail.usu.edu>
2018-04-01 19:12:15 -0600

    Changed Snake speed a little.

--- src/components/Snake/SnakeScreen.js
+++ src/components/Snake/SnakeScreen.js
@@ -105,7 +105,7 @@ export default class SnakeScreen extends Component {
       this.intervalId = setInterval(()=>{
         this.MoveSnake();
         this.forceUpdate();
-      }, 200);
+      }, 250);
     }
 
     render() {
